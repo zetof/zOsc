e1283f7865eeb82ab7f35cf13131e9768949cdee
Christophe Bury <christophe.bury@netaxis.be>
2019-07-08 22:33:04 +0200

    Vslider component moved to a dedicated directory - better import with kv file syntax

--- zosc.py
+++ zosc.py
@@ -1,45 +1,19 @@
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
-from kivy.properties import NumericProperty
-from kivy.properties import StringProperty
 from client.osc import OscClient
+from kivy.lang import Builder
+
+Builder.load_file('components/vslider.kv')
+
 
 class Table(GridLayout):
     widget_name = None
     osc = OscClient('127.0.0.1', 57120)
 
-class Vslider(GridLayout):
-    widget_name = StringProperty()
-    widget_value = NumericProperty()
-
-    def on_touch_down(self, touch):
-        parent = self.parent
-        if self.collide_point(*touch.pos):
-            parent.widget_name = self.widget_name
-            value = round(100 * (touch.y - self.y) / self.height)
-            self.widget_value = value
-            parent.osc.send('foobar', self.widget_name, value)
-
-    def on_touch_move(self, touch):
-        parent = self.parent
-        if self.widget_name == parent.widget_name:
-            value = round(100 * (touch.y - self.y) / self.height)
-            if value < 0:
-                value = 0
-            elif value > 100:
-                value = 100
-            self.widget_value = value
-            parent.osc.send('foobar', self.widget_name, value)
-
-    def on_touch_up(self, touch):
-        parent = self.parent
-        parent.widget_name = None
-
 
 class ZoscApp(App):
     def build(self):
         return Table()
-
 
 if __name__ == '__main__':
     ZoscApp().run()
