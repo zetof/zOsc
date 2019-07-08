eec29ea3bb1de1f4eb6e8c31655500a3340fc746
Christophe Bury <christophe.bury@netaxis.be>
2019-07-08 13:38:58 +0200

    First version of working sliders in Kivy - without OSC integration

---
+++ zosc.py
@@ -0,0 +1,39 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.gridlayout import GridLayout
+from kivy.properties import NumericProperty
+from kivy.properties import StringProperty
+
+
+class Table(GridLayout):
+    widget_name = None
+
+
+class Vslider(Widget):
+    widget_name = StringProperty()
+    widget_value = NumericProperty()
+
+    def on_touch_down(self, touch):
+        if self.collide_point(*touch.pos):
+            Table.widget_name = self.widget_name
+            self.widget_value = round(100 * (touch.y - self.y) / self.height)
+
+    def on_touch_move(self, touch):
+        if self.widget_name == Table.widget_name:
+            value = round(100 * (touch.y - self.y) / self.height)
+            if value < 0:
+                value = 0
+            elif value > 100:
+                value = 100
+            self.widget_value = value
+            print(self.widget_value)
+
+    def on_touch_up(self, touch):
+        Table.widget_name = None
+
+class ZoscApp(App):
+    def build(self):
+        return Table()
+
+if __name__ == '__main__':
+    ZoscApp().run()
